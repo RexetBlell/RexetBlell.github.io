9bac65fc5c155570d7c4510e9c1542bc12fd3325
Rexet Blell <rexetblell@gmail.com>
2016-07-17 21:34:55 -0700

    Implement move making

--- script.js
+++ script.js
@@ -21,48 +21,67 @@ $(function(){
     var connectsix_contract = web3.eth.contract(contractABI);
     var connectsix = connectsix_contract.at(contract_address);
 
-    for (var x = 0; x < 19; x++) {
-        for (var y = 0; y < 19; y++) {
-            var r = connectsix.board(x, y);
-            if (r == 1) {
-                board.addObject({
-                    x: x,
-                    y: y,
-                    c: WGo.B
-                });
-            } else if (r == 2) {
-                board.addObject({
-                    x: x,
-                    y: y,
-                    c: WGo.W
-                });
+    var my_moves = [];
+
+    var refresh = function() {
+        for (var x = 0; x < 19; x++) {
+            for (var y = 0; y < 19; y++) {
+                var r = connectsix.board(x, y);
+                board.removeObjectsAt(x, y);
+                if (r == 1) {
+                    board.addObject({
+                        x: x,
+                        y: y,
+                        c: WGo.B
+                    });
+                } else if (r == 2) {
+                    board.addObject({
+                        x: x,
+                        y: y,
+                        c: WGo.W
+                    });
+                }
             }
+        }
+        turn = connectsix.turn();
+        var color;
+        if (turn == 1) {
+            color = WGo.B;
+        } else {
+            color = WGo.W;
+        }
+        for (var i = 0; i < my_moves.length; i++) {
+            if (connectsix.board(my_moves[i].x, my_moves[i].y) != 0) {
+                my_moves = [];
+                break;
+            }
+            board.addObject({
+                x: my_moves[i].x,
+                y: my_moves[i].y,
+                c: color
+            });
+            board.addObject({
+                x: my_moves[i].x,
+                y: my_moves[i].y,
+                c: "CR"
+            });
         }
     }
 
+    refresh();
 
-    /*
     board.addEventListener("click", function(x, y) {
-        if(turn == 1) {
-            board.addObject({
-                x: x,
-                y: y,
-                c: WGo.B
-            });
-        }
-        else if(turn == 2) {
-            board.addObject({
-                x: x,
-                y: y,
-                c: WGo.W
-            });
-        }
-        if (moves_left == 1) {
-            turn = 3 - turn;
-            moves_left = 2;
+        if (my_moves.length >= 2) {
+            my_moves = [];
         } else {
-            moves_left = 1;
+            for (var i = 0; i < my_moves.length; i++) {
+                if (my_moves[i].x == x && my_moves[i].y == y) {
+                    my_moves = [];
+                    break;
+                }
+            }
+            my_moves.push({x:x, y:y});
         }
+        refresh();
     });
-    */
 });
